8d0ec4292dac2c505d62df7fd0ca808c78a65d5e
teoreiman <teo@reiman.com.ar>
2025-05-29 11:58:45 -0300

    sphere en la camara SIN mesh renderer

---
+++ Assets/deteccion_de_coliciones.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deteccion_de_coliciones : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Contacto");
+        Destroy(collision.gameObject);
+    }
+}
---
+++ Assets/mercanciaScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mercanciaScript : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
