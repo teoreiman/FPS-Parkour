f43d62bb8455dd880cf1ad7a12d73e73eaef1034
teoreiman <teo@reiman.com.ar>
2025-06-05 12:10:40 -0300

    vinculacion txt money manager, mercancia

---
+++ Assets/Scripts/mercanciascript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mercanciascript : MonoBehaviour
+{
+    public float cost;
+    public moneyManager MoneyManager;
+
+ void Start()
+    {
+        MoneyManager = FindObjectOfType<moneyManager>();
+    }
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            // actualizar dinero por la compra
+            MoneyManager.UpdateMoney(-cost);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/moneyManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moneyManager : MonoBehaviour
+{
+    public float money;
+    public UI_MANAGER uiManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiManager.UpdateMoneyTxt(money.ToString());
+    }
+
+        public void UpdateMoney(float moneyToAdd)
+    {
+        if (money + moneyToAdd < 0)
+        {
+            Debug.Log("Dinero insuficiente");
+            return;
+        }
+        money += moneyToAdd;
+        uiManager.UpdateMoneyTxt(money.ToString());
+    }
+}
